a97672ad16897c14068f2c845d65f3488137e90e
Riad <mzriad12@gmail.com>
2023-11-13 03:34:31 +0300

    Adding main files for Stepik Final Test

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,14 @@
 from selenium.webdriver.common.by import By
 
+class MainPage():
+
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
